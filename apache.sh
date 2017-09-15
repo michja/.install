b9412082a0be22eb13655ab27e152885fea425cb
Michael James <30815899+michja@users.noreply.github.com>
2017-09-15 10:58:49 +0100

    Create apache.sh

---
+++ apache.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# currently post install, needs install steps adding
+
+a2enmod rewrite
+
+apt install php-pear
+pear install mail
+
+# php enable mods - write to /etc/php/7.0/mods-available/
+# then phpendmod
